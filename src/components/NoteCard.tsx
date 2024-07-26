c4781b716698696dfef692e69d1fbd65134b79bd
skies-codes <akashdebnathwd@gmail.com>
2024-07-26 17:40:58 +0530

    dynamic card position by using onMouseDown, mousemove, mouseup

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { Note } from "../types/types";
 import Trash from "../icons/Trash";
 
@@ -7,7 +7,10 @@ interface NoteCardTypes {
 }
 
 const NoteCard: FC<NoteCardTypes> = ({ note }) => {
+    const [position, setPositon] = useState(note.position);
+
     const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+    const cardRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         autoGrow(textAreaRef);
@@ -23,18 +26,56 @@ const NoteCard: FC<NoteCardTypes> = ({ note }) => {
         }
     }
 
+    // draggable card position logic
+    let mouseStartPos = { x: 0, y: 0 };
+
+    const mouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
+    };
+
+    const mouseMove = (e: MouseEvent) => {
+        //1 - Calculate move direction
+        let mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+
+        //2 - Update start position for next move.
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        //3 - Update card top and left position.
+        if (cardRef.current) {
+            setPositon({
+                x: cardRef.current.offsetLeft - mouseMoveDir.x,
+                y: cardRef.current.offsetTop - mouseMoveDir.y,
+            });
+        }
+    };
+
+    const mouseUp = () => {
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp);
+    };
+
     return (
         <div
+            ref={cardRef}
             className='card'
             style={{
                 backgroundColor: note.colors.colorBody,
-                left: `${note.position.x}px`,
-                top: `${note.position.y}px`,
+                left: `${position.x}px`,
+                top: `${position.y}px`,
             }}
         >
             <div
                 className='card-header'
                 style={{ backgroundColor: note.colors.colorHeader }}
+                onMouseDown={(e) => mouseDown(e)}
             >
                 <Trash size='24' />
             </div>
